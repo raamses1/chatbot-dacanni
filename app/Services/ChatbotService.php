cae05a60b8870b771141a83c1311f97c969a0a91
Raamses Chavez Sanchez <raamseschavez21@gmail.com>
2026-02-04 19:08:43 -0600

    Migracion en tabla user chats y correccion de waiting_product_price

--- app/Services/ChatbotService.php
+++ app/Services/ChatbotService.php
@@ -45,6 +45,26 @@ class ChatbotService
 
         if ($detectedProduct) {
             $userChat->last_product = $detectedProduct;
+            $userChat->save();
+        }
+
+        if($userChat->conversation_state === 'waiting_product_price'){
+            if($detectedProduct){
+                $userChat->conversation_state = null;
+                $userChat->last_product = $detectedProduct;
+                $userChat->save();
+
+                return [
+                    'reply' => 'La'. $detectedProduct .'cuesta $5000 mxn',
+                    'intent' => 'precio',
+                    'score' => 1
+                ];
+            }
+            return [
+                'reply' => '¿de que producto quieres saber el precio',
+                'intent' => null,
+                'score' => 0
+            ];
         }
 
         // detectar intent
@@ -113,6 +133,13 @@ class ChatbotService
     //imprimir la respuesta final
     private function buildReply(?string $intent, $userChat): string
     {
+        //precio sin producto
+        if($intent === 'precio' &&!$userChat->last_product){
+            $userChat->conversation_state = 'waiting_product_price';
+            $userChat->save();
+
+            return '¿de que producto quieres saber el precio?';
+        }
         // precio con producto
         if ($intent === 'precio' && $userChat->last_product) {
 
